30e3a0f25214ebafa6aea739a1c4fd7d72ccf427
JaredAcosta0 <58441808+JaredAcosta0@users.noreply.github.com>
2021-05-26 14:45:21 -0500

    Nuevo o

---
+++ formulario.php
@@ -0,0 +1,52 @@
+<?php
+    require_once 'vendor/autoload.php';
+
+    use Illuminate\Database\Capsule\Manager as Capsule;
+    use App\Models\Project;
+
+    $capsule = new Capsule;
+    
+    $capsule->addConnection([
+        'driver'    => 'mysql',
+        'host'      => '127.0.0.1:3306',
+        'database'  => 'curvit',
+        'username'  => 'root',
+        'password'  => 'root',
+        'charset'   => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix'    => '',
+    ]);
+
+    // Make this Capsule instance available globally via static methods... (optional)
+    $capsule->setAsGlobal();
+
+    // Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+    $capsule->bootEloquent();
+
+    if(!empty($_POST)){
+        $project = new Project();
+        $project -> title = $_POST['title'];
+        $project -> description = $_POST['description'];
+        $project -> save();
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Formulrio Job</h1>
+    <form action="formulario.php" method="post">
+        <label for="">Title: </label>
+        <input type="text" name="title"><br>
+        <label for="">Description: </label>
+        <input type="text" name="description"><br>
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
